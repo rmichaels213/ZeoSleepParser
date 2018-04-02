cda20c729ad8c9e183d84669dedc3cd6e508862b
Ryan - GAMESTOPIA <rmichaels213@gmail.com>
2018-04-02 02:23:06 -0400

    Start of parsing.
    Have header reacords found. Need to work on a way to store subsequent data rows.

--- ZeoSleepParser/ZeoSleepParser/MainPage.xaml.cs
+++ ZeoSleepParser/ZeoSleepParser/MainPage.xaml.cs
@@ -6,6 +6,8 @@ Update Date:	03/30/2018
 ---------------------------------------- */
 
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -16,12 +18,17 @@ namespace ZeoSleepParser
 	/// </summary>
 	public sealed partial class MainPage : Page
 	{
-
 		private Windows.Storage.StorageFile fileToParse;
+
+		private List<string> header;
+		private List<string[]> data;
 
 		public MainPage()
 		{
 			this.InitializeComponent();
+
+			header = new List<string>();
+			data = new List<string[]>();
 
 			// Start things up!
 			StartMainApp();
@@ -66,15 +73,112 @@ namespace ZeoSleepParser
 		/// <param name="e"></param>
 		private async void ButtonParse_Click(object sender, RoutedEventArgs e)
 		{
-			if (fileToParse == null )
+			const char DELIMETER = ',';
+			const char NEWLINE_DELIMETER = '\n';
+			const char CARRIAGE_RETURN_DELIMETER = '\r';
+
+			textError.Text = "Loading file.";
+			string fileText = await ReadFileAsync();
+			textError.Text = "File loaded.";
+
+			if (string.IsNullOrWhiteSpace(fileText))
 			{
-				textError.Text = "No file selected.";
-				textParse.Text = string.Empty;
+				textError.Text = "Invalid file.";
 				return;
 			}
 
-			string fileText = await Windows.Storage.FileIO.ReadTextAsync(fileToParse);
-			textParse.Text = fileText;
+			bool isHeaderFound = false;
+
+			int fileLength = fileText.Length;
+
+			int lastPosition = 0;
+			int position = 0;
+
+			int dataRow = 0;
+			int wordCount = 0;
+
+
+			textError.Text = "Starting to parse file.";
+			while ( position < fileLength)
+			{
+				// Find a new line
+				if (fileText[position] == CARRIAGE_RETURN_DELIMETER)
+				{
+					// Check next character
+					if (fileText[position + 1] == NEWLINE_DELIMETER)
+					{
+						textError.Text = "Newline found";
+						isHeaderFound = true;
+
+						// Move the data row to the next
+						dataRow++;
+						position++;
+
+						continue;
+					}
+				}
+
+				if (fileText[position] == DELIMETER)
+				{
+					textError.Text = "Found delimeter!";
+					if ( !isHeaderFound )
+					{
+						// Keep adding to header record if we haven't found the end yet
+						header.Add(fileText.Substring(lastPosition, position - lastPosition));
+					}
+					else
+					{
+						//data[dataRow].Add(fileText.Substring(lastPosition, position - lastPosition));
+					}
+
+					// Swap positions
+					lastPosition = position + 1;
+					wordCount++;
+				}
+
+				position++;
+			}
+
+			textError.Text = "Parsing done!";
+		}
+		
+		/// <summary>
+		/// Asynchronous method to read in file
+		/// </summary>
+		private async Task<string> ReadFileAsync()
+		{
+			if (fileToParse == null)
+			{
+				textError.Text = "No file selected.";
+				textParse.Text = string.Empty;
+				return string.Empty;
+			}
+
+			return await Windows.Storage.FileIO.ReadTextAsync(fileToParse);
+		}
+
+		private void UpdateParseText()
+		{
+			int parseTextPosition = 0;
+
+			switch (parseTextPosition)
+			{
+				case 0:
+					textError.Text = "Parsing";
+					break;
+				case 1:
+					textError.Text = "Parsing.";
+					break;
+				case 2:
+					textError.Text = "Parsing..";
+					break;
+				case 3:
+				default:
+					textError.Text = "Parsing...";
+					break;
+			}
+
+			parseTextPosition = parseTextPosition == 3 ? 0 : parseTextPosition++;
 		}
 	}
 }
